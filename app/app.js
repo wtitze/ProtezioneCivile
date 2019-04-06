2120261f74a37b02e8b15871d2ed6b0e4c8e27c6
wtitze <w.titze@gmail.com>
2019-04-06 16:12:42 +0000

    salvataggio delle segnalazioni su MongoDB

--- app/app.js
+++ app/app.js
@@ -3,6 +3,8 @@
 var express = require('express');
 var app = express();
 
+var MongoClient = require('mongodb').MongoClient;
+
 // configurazione motore di render
 
 app.set('views', './views');
@@ -55,7 +57,21 @@ io.on('connection', function(socket){ // gestisce le connessioni
     // e per spedire la segnalazione a tutti i volontari connessi ad eccezione del mittente
     
     console.log('posizione: ' + msg.posizione.lat + ' ' + msg.posizione.lng); // sulla console viene visualizzato il messaggio ricevuto
-    socket.broadcast.emit('segnalazione', msg); // spedito a tutti tranne al mittente
+    socket.broadcast.emit('segnalazione', msg); // spedito a tutti tranne che al mittente
+    
+    // memorizzazione nel database di MongoDB
+    MongoClient.connect('mongodb+srv://admin:MwbZUn1JUfbuRoSK@galvani-c4mon.mongodb.net/?retryWrites=true,{useNewUrlParser: true}', function(err, db) {
+      if (err) {
+        throw err;
+      }
+      var dbo = db.db("5E");
+      var newInfo = {data: new Date(Date.now()).toISOString(), lat: msg.posizione.lat, lng: msg.posizione.lng, indirizzo: msg.indirizzo };
+      dbo.collection("ProtezioneCivile").insertOne(newInfo, function(err, result) {
+        if (err) throw err;
+        db.close();
+      });
+    });
+    
   });
   
   socket.on('disconnect', function(){ // quando viene ricevuto l'evento 'disconnect'
